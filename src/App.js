e93ff7ceaf6e0206122ea1153478c455dce48fad
david_oluwasusi <daviddamilola20@gmail.com>
2020-11-29 00:56:26 +0100

    implementation in progress

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { data } from './data';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 function App() {
+  const [list, setList] = useState(data);
+
+  const onDragEnd = (result) => {
+    const { destination, source, reason } = result;
+
+    if (!destination || reason === 'CANCEL') {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId ||
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const users = [...list];
+    const droppedUser = list[source.index];
+    users.splice(source.index, 1);
+    users.splice(destination.index, 0, droppedUser);
+    setList(users);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <UserList list={list} />
+      </DragDropContext>
     </div>
   );
 }
+
+function UserList({ list }) {
+  return (
+    <>
+      <h2 style={{ textIndent: '1.5em' }}>User List</h2>
+      <ol style={{ listStyle: 'none', height: '600px' }}>
+        <Droppable droppableId='dp1'>
+          {(provided) => (
+            <div ref={provided.innerRef} {...provided.droppableProps}>
+              {list.map((each, index) => (
+                <Draggable key={index+''} draggableId={index + ''} index={index}>
+                  {(provided) => (
+                    <div
+                    key={index}
+                      ref={provided.innerRef}
+                      {...provided.droppableProps}
+                      {...provided.dragHandleProps}
+                    >
+                      <User index={index + 1} user={each} />
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </ol>
+    </>
+  );
+}
+
+const User = ({ user, index }) => {
+  return (
+    <li
+      style={{
+        padding: '5px 10px',
+        borderRadius: '4px',
+        border: '1px solid grey',
+        marginBottom: '0.5em',
+        width: '12rem',
+      }}
+    >
+      <div style={{ display: 'flex' }}>
+        <h3 style={{ width: '12px', display: 'inline', marginRight: '1rem' }}>
+          {' '}
+          {index}{' '}
+        </h3>
+        <div>
+          <p>{user.name}</p>
+          <p>{user.age}</p>
+        </div>
+      </div>
+    </li>
+  );
+};
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,16 @@
+export const data = [{
+    name: 'tunde awati',
+    age: '22'
+},
+{
+    name: 'mawami awati',
+    age: '23'
+},
+{
+    name: 'alonge awati',
+    age: '24'
+},
+{
+    name: 'last name',
+    age: '24'
+}]
